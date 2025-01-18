cf25491db01ea01823d147a7e5a50cc2a1a84995
Chris <chris@breadchris.com>
2025-01-17 17:48:56 -0800

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: "/phs-alumni-site",
+    basePath: "",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: { unoptimized: true }
